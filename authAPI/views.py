427ce379e7bb15c1d5c2e4b0ff51e869519031ec
Mikias Miessa <mikiasmiessa@gmail.com>
2023-08-02 12:51:53 +0300

    some touches

--- authAPI/views.py
+++ authAPI/views.py
@@ -3,6 +3,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+
 from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
@@ -14,12 +15,16 @@ def register_user(request):
     password = request.data.get('password')
     username = request.data.get('username')
     
-    if not email or not password:
-        return JsonResponse({'error': 'Email and password are required.'}, status=400)
+    if not email or not password or not username:
+        return JsonResponse({'error': 'All fields are required.'}, status=400)
 
     # Check email format
     if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
         return JsonResponse({'error': 'Invalid email format.'}, status=400)
+
+    # Check for white spaces in username
+    if ' ' in username:
+        return JsonResponse({'error': 'Username can not contain white spaces.'}, status=400)
 
     # Check if the email is already registered
     if User.objects.filter(username=username).exists():
@@ -30,17 +35,23 @@ def register_user(request):
     # Create a new user
     user = User.objects.create_user(username=username, email=email, password=password)
     return JsonResponse({'message': 'Registration successful.'}, status=201)
-
 @api_view(['POST'])
-def login_user(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
-    if not email or not password:
-        return JsonResponse({'error': 'Email and password are required.'}, status=400)
 
-    user = authenticate(username=email, password=password)
+
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    
+    # Remove white spaces from username and password
+    username = username.strip() if username else username
+    password = password.strip() if password else password
+    
+    if not username or not password:
+        return JsonResponse({'error': 'username and password are required.'}, status=400)
+
+    user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
         return JsonResponse({'message': 'Login successful.'}, status=200)
     else:
-        return JsonResponse({'error': 'Invalid email or password.'}, status=401)
+        return JsonResponse({'error': 'Invalid username or password.'}, status=401)
